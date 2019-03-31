7524a186890e8e7e8c6a8e2923d27bb0c71ef193
Igor Pires Ferreira <ipferreira@inf.ufrgs.br>
2019-03-31 10:51:07 -0300

    Building templating structure

--- bootstrap.php
+++ bootstrap.php
@@ -4,10 +4,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 function buildContainer() : DI\Container {
     $containerBuilder = new DI\ContainerBuilder();
     $containerBuilder->addDefinitions('config.php');
+    return $containerBuilder->build();
 }
 
 function buildTwig() : \Twig\Environment {
-    $loader = new \Twig\Loader\FilesystemLoader('view/');
+    $loader = new \Twig\Loader\FilesystemLoader([
+        'view/', 'view/templates/'
+    ]);
     return new \Twig\Environment($loader, [
             'cache' => 'cache/',
             'debug' => true,
